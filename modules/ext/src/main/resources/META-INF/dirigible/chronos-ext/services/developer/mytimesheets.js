c4bab2dffed9e904f0f91f794985cf7a9fa02191
delchev <delchevn@gmail.com>
2022-10-13 10:50:49 +0000

    continues

--- modules/ext/src/main/resources/META-INF/dirigible/chronos-ext/services/developer/mytimesheets.js
+++ modules/ext/src/main/resources/META-INF/dirigible/chronos-ext/services/developer/mytimesheets.js
@@ -19,20 +19,20 @@ let statusFilter = "";
 let statusIds = request.getParameterValues('StatusId');
 if (statusIds && statusIds.length) {
     statusIds = statusIds.map(id => parseInt(id));
-    statusFilter = ` AND TIMESHEET_STATUS IN ( ${statusIds.map(() => '?').join(',')} )`;
+    statusFilter = ` AND "TIMESHEET_STATUS" IN ( ${statusIds.map(() => '?').join(',')} )`;
 }
 
 var sql = 'SELECT * FROM "CHRONOS_TIMESHEET", "CHRONOS_ITEM", "CHRONOS_TASK", "CHRONOS_EMPLOYEE", "CHRONOS_TIMESHEETSTATUS" '
     + ' WHERE "EMPLOYEE_EMAIL" = ? '
-    + ' AND TIMESHEET_PROJECTID = ? '
-    + ' AND TIMESHEET_ID = ITEM_TIMESHEETID '
-    + ' AND TASK_ID = ITEM_TASKID '
-    + ' AND EMPLOYEE_ID = TIMESHEET_EMPLOYEEID '
-    + ' AND TIMESHEET_STATUS = TIMESHEETSTATUS_ID'
+    + ' AND "TIMESHEET_PROJECTID" = ? '
+    + ' AND "TIMESHEET_ID" = "ITEM_TIMESHEETID" '
+    + ' AND "TASK_ID" = "ITEM_TASKID" '
+    + ' AND "EMPLOYEE_ID" = "TIMESHEET_EMPLOYEEID" '
+    + ' AND "TIMESHEET_STATUS" = "TIMESHEETSTATUS_ID" '
     + statusFilter
-    + ' ORDER BY TIMESHEET_ID';
+    + ' ORDER BY "TIMESHEET_ID"';
 
-let projectId = request.getParameter('ProjectId');
+let projectId = parseInt(request.getParameter('ProjectId'));
 
 if (projectId) {
     let d = new Date();
