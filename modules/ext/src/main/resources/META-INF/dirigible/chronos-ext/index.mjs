08f5b772de38b882c6aef448e1bee22a6153ef27
Yordan Pavlov <ThuF@users.noreply.github.com>
2022-12-02 15:52:12 +0200

    Update index.mjs

--- modules/ext/src/main/resources/META-INF/dirigible/chronos-ext/index.mjs
+++ modules/ext/src/main/resources/META-INF/dirigible/chronos-ext/index.mjs
@@ -7,6 +7,8 @@ if (user.isInRole("Employee")) {
     response.sendRedirect("/services/v4/web/chronos-ext/ui/manager/");
 } else if (user.isInRole("Admin")) {
     response.sendRedirect("/services/v4/web/chronos-app/gen/");
+} else if (user.isInRole("Developer") || user.isInRole("Operator")) {
+    response.sendRedirect("/services/v4/web/ide/");
 } else {
     response.sendError(response.FORBIDDEN, "Unathorized access");
 }
