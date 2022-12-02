3c94716d8a138710fbd850a585d43cefcad97ade
Yordan Pavlov <ThuF@users.noreply.github.com>
2022-12-02 15:43:16 +0200

    Update index.mjs

--- modules/ext/src/main/resources/META-INF/dirigible/chronos-ext/index.mjs
+++ modules/ext/src/main/resources/META-INF/dirigible/chronos-ext/index.mjs
@@ -1,10 +1,12 @@
 import { response } from "@dirigible/http";
 import { user } from "@dirigible/security";
 
-if (user.isInRole("Manager")) {
-    response.sendRedirect("/services/v4/web/chronos-ext/ui/manager/index.html");
-} else if (user.isInRole("Employee")) {
-    response.sendRedirect("/services/v4/web/chronos-ext/ui/developer/index.html");
+if (user.isInRole("Employee")) {
+    response.sendRedirect("/services/v4/web/chronos-ext/ui/developer/");
+} else if (user.isInRole("Manager")) {
+    response.sendRedirect("/services/v4/web/chronos-ext/ui/manager/");
+} else if (user.isInRole("Admin")) {
+    response.sendRedirect("/services/v4/web/chronos-app/gen/");
 } else {
     response.sendError(response.FORBIDDEN, "Unathorized access");
 }
